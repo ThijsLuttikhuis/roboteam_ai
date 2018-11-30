a79bbeb63a1d2318d3939447af4b7a82de4ee2c0
simenbootsma <s.t.bootsma@student.utwente.nl>
2018-11-30 15:36:27 +0100

    Increase max velocity

--- roboteam_ai/src/control/pathFinder/CurveCreator.h
+++ roboteam_ai/src/control/pathFinder/CurveCreator.h
@@ -16,7 +16,7 @@ namespace rtt {
         private:
             // Variables
             float robotDiameter = 0.18; // TODO: Should be pulled from some list with constants
-            float maxVelocity = 1.0; // maximum absolute velocity TODO: Should be pulled from some list with constants
+            float maxVelocity = 1.5; // maximum absolute velocity TODO: Should be pulled from some list with constants
             std::vector<Vector2> curveAccelerations;
             std::vector<Vector2> curveVelocities;
             std::vector<float> curveOrientations;
